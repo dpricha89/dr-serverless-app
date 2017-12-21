cc8fe58682e7c6785271fb8bc2b2112d0cba19f9
David Richards <dpricha189@gmail.com>
2017-12-21 09:13:19 -0800

    Added error response wrapper

--- src/utls/helpers.js
+++ src/utls/helpers.js
@@ -3,16 +3,25 @@
 const Sumo = require('../lib/sumo.js')
 
 class Helpers {
-  successResponse (callback, body) {
+  successResponse (callback, body, statusCode = 200) {
     const resBody = body || {
       status: 'success'
     }
     callback(null, {
-      statusCode: 200,
+      statusCode: statusCode,
       headers: {
         'Access-Control-Allow-Origin': '*' // Required for CORS support to work
       },
       body: JSON.stringify(resBody)
+    })
+  }
+
+  errorResponse (callback, err, statusCode = 500) {
+    callback(JSON.stringify(err), {
+      statusCode: statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+      }
     })
   }
 
